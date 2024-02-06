3a5c3f37a2aa7a817cbec60631dade2c6c6ea981
Subaida Begum <subaida@transvelo.com>
2024-02-06 13:56:23 +0530

    Fixes linting errors in main file

--- mas-wp-job-manager-company.php
+++ mas-wp-job-manager-company.php
@@ -13,8 +13,6 @@
  * Domain Path:       /languages
  *
  * @package MAS Companies For WP Job Manager
- * @category Core
- * @author MadrasThemes
  */
 
 // Exit if accessed directly.
@@ -27,21 +25,24 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Main MAS_WPJMC class initializes the plugin.
  *
- * @class     MAS_WP_Job_Manager_Company
- * @version   1.0.0
- * @author    Madras Themes
+ * @class   MAS_WP_Job_Manager_Company
+ * @version 1.0.0
  */
 class MAS_WP_Job_Manager_Company {
 
 	/**
-	 * Instace of MAS_WP_Job_Manager_Company.
+	 * Instance of MAS_WP_Job_Manager_Company.
 	 *
 	 * @since 1.0.0
-	 * @access private
 	 * @var object $instance The instance of MAS_WPJMC.
 	 */
 	private static $instance;
 
+	/**
+	 * Version of the plugin.
+	 *
+	 * @var string $version
+	 */
 	public $version = '1.0.8';
 
 	/**
@@ -160,39 +161,39 @@ class MAS_WP_Job_Manager_Company {
 		load_plugin_textdomain( dirname( plugin_basename( __FILE__ ) ), false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 		// Functions.
-		require( $this->plugin_dir . 'includes/mas-wpjmc-functions.php' );
-		require( $this->plugin_dir . 'includes/mas-wpjmc-template-functions.php' );
+		require $this->plugin_dir . 'includes/mas-wpjmc-functions.php';
+		require $this->plugin_dir . 'includes/mas-wpjmc-template-functions.php';
 
 		// Template Hooks.
-		require( $this->plugin_dir . 'includes/mas-wpjmc-template-hooks.php' );
+		require $this->plugin_dir . 'includes/mas-wpjmc-template-hooks.php';
 
 		// Abstracts Class
-		include_once( $this->plugin_dir . 'includes/abstracts/abstract-mas-wp-job-manager-company-email-template.php' );
+		include_once $this->plugin_dir . 'includes/abstracts/abstract-mas-wp-job-manager-company-email-template.php';
 
-		 //Admin Class
-		include_once( $this->plugin_dir . 'includes/admin/class-wp-job-manager-company-admin.php' );
+		// Admin Class
+		include_once $this->plugin_dir . 'includes/admin/class-wp-job-manager-company-admin.php';
 
 		/* === CLASSES === */
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company.php';
 		$this->company = new MAS_WPJMC();
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-cpt.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-cpt.php';
 		$this->cpt = new MAS_WPJMC_CPT();
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-shortcode.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-shortcode.php';
 		$this->shortcode = new MAS_WPJMC_Shortcode();
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-forms.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-forms.php';
 		$this->forms = new MAS_WPJMC_Forms();
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-query.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-query.php';
 		$this->query = new MAS_WPJMC_Query();
 
-		require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-template-loader.php' );
+		require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-template-loader.php';
 
-		if( is_admin() ) {
-			require( $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-writepanels.php' );
+		if ( is_admin() ) {
+			require $this->plugin_dir . 'includes/class-mas-wp-job-manager-company-writepanels.php';
 			$this->writepanels = new MAS_WPJMC_Writepanels();
 		}
 
@@ -213,7 +214,9 @@ class MAS_WP_Job_Manager_Company {
 		wp_register_script( 'mas-wp-job-manager-company-submission', plugins_url( 'assets/js/company-submission.min.js', __FILE__ ), array( 'jquery', 'jquery-ui-sortable' ), MAS_JOB_MANAGER_COMPANY_VERSION, true );
 
 		wp_localize_script(
-			'mas-wp-job-manager-company-submission', 'mas_wp_job_manager_company_submission', array(
+			'mas-wp-job-manager-company-submission',
+			'mas_wp_job_manager_company_submission',
+			array(
 				'i18n_navigate'       => esc_html__( 'If you wish to edit the posted details use the "edit resume" button instead, otherwise changes may be lost.', 'mas-wp-job-manager-company' ),
 				'i18n_confirm_remove' => esc_html__( 'Are you sure you want to remove this item?', 'mas-wp-job-manager-company' ),
 				'i18n_remove'         => esc_html__( 'remove', 'mas-wp-job-manager-company' ),
@@ -221,21 +224,25 @@ class MAS_WP_Job_Manager_Company {
 		);
 
 		wp_localize_script(
-			'mas-wp-job-manager-company-dashboard', 'mas_wp_job_manager_company_dashboard', array(
+			'mas-wp-job-manager-company-dashboard',
+			'mas_wp_job_manager_company_dashboard',
+			array(
 				'i18n_confirm_delete' => esc_html__( 'Are you sure you want to delete this resume?', 'mas-wp-job-manager-company' ),
 			)
 		);
 
-		// General stylesheet.
-		if( apply_filters( 'mas_wpjmc_enqueue_scripts_enable_frontend_css', true ) ) {
-			wp_enqueue_style( 'mas-wp-job-manager-company-frontend', plugins_url( 'assets/css/frontend.css', __FILE__ ), array( 'dashicons' ) );
+		/**
+		 * Enable disable frontend CSS.
+		 */
+		if ( apply_filters( 'mas_wpjmc_enqueue_scripts_enable_frontend_css', true ) ) {
+			wp_enqueue_style( 'mas-wp-job-manager-company-frontend', plugins_url( 'assets/css/frontend.css', __FILE__ ), array( 'dashicons' ), $this->version );
 			wp_style_add_data( 'mas-wp-job-manager-company-frontend', 'rtl', 'replace' );
 		}
 	}
 
 	public function mas_wpjmc_admin_enqueue_scripts() {
 		$screen = get_current_screen();
-		if ( in_array( $screen->id, apply_filters( 'mas_wpjmc_admin_screen_ids', [ 'edit-company', 'plugins', 'company' ] ), true ) ) {
+		if ( in_array( $screen->id, apply_filters( 'mas_wpjmc_admin_screen_ids', array( 'edit-company', 'plugins', 'company' ) ), true ) ) {
 			$script_deps = array( 'jquery', 'jquery-tiptip' );
 
 			if ( wp_script_is( 'select2', 'registered' ) ) {
@@ -252,18 +259,18 @@ class MAS_WP_Job_Manager_Company {
 			wp_localize_script(
 				'job_manager_admin_js',
 				'job_manager_admin_params',
-				[
-					'user_selection_strings' => [
+				array(
+					'user_selection_strings' => array(
 						'no_matches'        => _x( 'No matches found', 'user selection', 'mas-wp-job-manager-company' ),
 						'ajax_error'        => _x( 'Loading failed', 'user selection', 'mas-wp-job-manager-company' ),
 						'input_too_short_1' => _x( 'Please enter 1 or more characters', 'user selection', 'mas-wp-job-manager-company' ),
 						'input_too_short_n' => _x( 'Please enter %qty% or more characters', 'user selection', 'mas-wp-job-manager-company' ),
 						'load_more'         => _x( 'Loading more results&hellip;', 'user selection', 'mas-wp-job-manager-company' ),
 						'searching'         => _x( 'Searching&hellip;', 'user selection', 'mas-wp-job-manager-company' ),
-					],
+					),
 					'ajax_url'               => admin_url( 'admin-ajax.php' ),
 					'search_users_nonce'     => wp_create_nonce( 'search-users' ),
-				]
+				)
 			);
 		}
 	}
@@ -275,7 +282,7 @@ class MAS_WP_Job_Manager_Company {
 	 * @return bool
 	 */
 	public function string_to_bool( $value ) {
-		return ( is_bool( $value ) && $value ) || in_array( $value, array( '1', 'true', 'yes' ) ) ? true : false;
+		return ( is_bool( $value ) && $value ) || in_array( $value, array( '1', 'true', 'yes' ) ) ? true : false; // phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict
 	}
 }
 
@@ -288,9 +295,10 @@ class MAS_WP_Job_Manager_Company {
  *
  * @return object MAS_WP_Job_Manager_Company class object.
  */
-function mas_wpjmc() {
-	if ( ! class_exists( 'WP_Job_Manager' ) )
+function mas_wpjmc() { // phpcs:ignore Universal.Files.SeparateFunctionsFromOO.Mixed
+	if ( ! class_exists( 'WP_Job_Manager' ) ) {
 		return;
+	}
 
 	return MAS_WP_Job_Manager_Company::instance();
 }
