8b4afee176b1bcc3f03f4b7aca4c382855ee277a
Subaida Begum <subaida@transvelo.com>
2024-02-19 23:32:55 +0530

    Update version

--- mas-wp-job-manager-company.php
+++ mas-wp-job-manager-company.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       MAS Companies For WP Job Manager
  * Description:       This plugin helps to create a custom post type company for WP Job Manager
- * Version:           1.0.9
+ * Version:           1.0.10
  * Requires at least: 6.3
  * Requires PHP:      7.4
  * Author:            MadrasThemes
@@ -43,7 +43,7 @@ class MAS_WP_Job_Manager_Company {
 	 *
 	 * @var string $version
 	 */
-	public $version = '1.0.9';
+	public $version = '1.0.10';
 
 	/**
 	 * Query instance.
